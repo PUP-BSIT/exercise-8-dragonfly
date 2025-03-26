5010ea2574e10ab5221fcdfceb35ef0c5d522774
barcelos-kevin <barceloskevinjoseph@gmail.com>
2025-03-26 15:53:24 +0800

    add function to add and show comment in the page but does not persist

--- barcelos_kevinjoseph/script.js
+++ barcelos_kevinjoseph/script.js
@@ -1,3 +1,4 @@
+// Comment disabled button
 let commentButton = document.getElementById("comment_button");
 let nameInput = document.getElementById("name_input");
 let messageInput = document.getElementById("message_input");
@@ -5,10 +6,42 @@ let messageInput = document.getElementById("message_input");
 commentButton.disabled = true;
 
 function toggleButtonState() {
-  let isNameFilled = nameInput.value.trim() !== "";
-  let isMessageFilled = messageInput.value.trim() !== "";
-  commentButton.disabled = !(isNameFilled && isMessageFilled);
+    let isNameFilled = nameInput.value.trim();
+    let isMessageFilled = messageInput.value.trim();
+    commentButton.disabled = !(isNameFilled && isMessageFilled);
 }
-
 nameInput.addEventListener("input", toggleButtonState);
 messageInput.addEventListener("input", toggleButtonState);
+
+// Add comment function
+function addComment(name, comment) {
+    let commentWrapper = document.createElement("div");
+    commentWrapper.classList.add("user-comment");
+
+    let nameElement = document.createElement("h4");
+    nameElement.classList.add("font-2");
+    nameElement.textContent = name;
+
+    let commentElement = document.createElement("p");
+    commentElement.textContent = "- " + comment;
+
+    commentWrapper.appendChild(nameElement);
+    commentWrapper.appendChild(commentElement);
+
+    let teamCommentSection = document.querySelector(".member-comments");
+    teamCommentSection.append(commentWrapper);
+
+    nameInput.value = "";
+    messageInput.value = "";
+
+    toggleButtonState();
+}
+
+commentButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    let name = nameInput.value.trim();
+    let comment = messageInput.value.trim();
+    if (name && comment) {
+        addComment(name, comment);
+    }
+});
