f6bd5a5e250faf68460e8203d7fe5cc19b5a8b15
gomez-ashleyhermione <hermionegomez49@gmail.com>
2025-03-26 15:29:22 +0300

    update gomez js file

--- gomez_ashleyhermione/script.js
+++ gomez_ashleyhermione/script.js
@@ -2,13 +2,33 @@ document.addEventListener("DOMContentLoaded", () => {
     let nameInput = document.getElementById("name");
     let commentInput = document.getElementById("comment");
     let submitButton = document.getElementById("submit_comment");
+    let commentsSection = document.getElementById("comments_section"); 
 
     function validateForm() {
-        submitButton.disabled = !(nameInput.value.trim() 
-        && commentInput.value.trim());
+        submitButton.disabled = !(
+            nameInput.value.trim() && commentInput.value.trim()
+        );
+    }
+
+    function addComment() {
+        let name = nameInput.value.trim();
+        let comment = commentInput.value.trim();
+
+        if (!name || !comment) return; 
+
+        let newComment = document.createElement("div");
+        newComment.classList.add("comment-item");
+
+        newComment.textContent = `${name}: ${comment}`; 
+
+        commentsSection.append(newComment);
+
+        nameInput.value = "";
+        commentInput.value = "";
+        validateForm();
     }
 
     nameInput.addEventListener("input", validateForm);
     commentInput.addEventListener("input", validateForm);
+    submitButton.addEventListener("click", addComment);
 });
-                                                                               
